fa698d5b7fd122dbf99a397805518ab5a2194e68
javinchua <javin.chua@gmail.com>
2023-01-16 14:17:25 +0800

    Update apiEndpoints.ts

--- utils/constants/apiEndpoints.ts
+++ utils/constants/apiEndpoints.ts
@@ -1,3 +1,3 @@
-const BACKEND_ROOT_URL = "http://localhost:8080/api";
+const BACKEND_ROOT_URL = "https://fusionwallet.herokuapp.com/api";
 const COIN_GECKO_URL = "https://api.coingecko.com/api/v3";
 export { BACKEND_ROOT_URL, COIN_GECKO_URL };
