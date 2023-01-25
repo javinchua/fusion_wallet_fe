e22b81dc86e031fc62d70d5bb8c10940faccf7af
javinchua <javin.chua@gmail.com>
2023-01-25 16:09:43 +0800

    Update signup.tsx

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -48,91 +48,92 @@ const Signup: NextPage = () => {
   };
   return (
     <Layout>
-      <Card
-        css={{
-          mw: "400px",
-          marginTop: "4rem",
-          marginLeft: "auto",
-          marginRight: "auto",
-        }}
-      >
-        <Card.Body>
-          <form
-            className="px-4 py-4"
-            onSubmit={(e) => {
-              handleSubmit(e);
-            }}
-          >
-            <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-            <Input
-              type="text"
-              className="mb-4"
-              fullWidth
-              name="username"
-              placeholder="Username"
-              value={signupDetails.username}
-              onChange={handleChange}
-            />
-            <Input
-              type="text"
-              className="mb-4"
-              fullWidth
-              name="email"
-              placeholder="Email"
-              value={signupDetails.email}
-              onChange={handleChange}
-            />
-            <Input
-              type="text"
-              className="mb-4"
-              fullWidth
-              name="firstName"
-              placeholder="First Name"
-              value={signupDetails.firstName}
-              onChange={handleChange}
-            />
-            <Input
-              type="text"
-              className="mb-4"
-              fullWidth
-              name="lastName"
-              placeholder="Last Name"
-              value={signupDetails.lastName}
-              onChange={handleChange}
-            />
-            <Input
-              type="password"
-              className="mb-4"
-              fullWidth
-              name="password"
-              placeholder="Password"
-              value={signupDetails.password}
-              onChange={handleChange}
-            />
-            <Input
-              type="password"
-              className="mb-4"
-              fullWidth
-              name="confirmPass"
-              placeholder="Confirm Password"
-              value={signupDetails.confirmPass}
-              onChange={handleChange}
-            />
+      <div className="md:w-[400px] w-[350px] mx-auto">
+        <Card
+          css={{
+            marginTop: "4rem",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <Card.Body>
+            <form
+              className="px-4 py-4"
+              onSubmit={(e) => {
+                handleSubmit(e);
+              }}
+            >
+              <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+              <Input
+                type="text"
+                className="mb-4"
+                fullWidth
+                name="username"
+                placeholder="Username"
+                value={signupDetails.username}
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                className="mb-4"
+                fullWidth
+                name="email"
+                placeholder="Email"
+                value={signupDetails.email}
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                className="mb-4"
+                fullWidth
+                name="firstName"
+                placeholder="First Name"
+                value={signupDetails.firstName}
+                onChange={handleChange}
+              />
+              <Input
+                type="text"
+                className="mb-4"
+                fullWidth
+                name="lastName"
+                placeholder="Last Name"
+                value={signupDetails.lastName}
+                onChange={handleChange}
+              />
+              <Input
+                type="password"
+                className="mb-4"
+                fullWidth
+                name="password"
+                placeholder="Password"
+                value={signupDetails.password}
+                onChange={handleChange}
+              />
+              <Input
+                type="password"
+                className="mb-4"
+                fullWidth
+                name="confirmPass"
+                placeholder="Confirm Password"
+                value={signupDetails.confirmPass}
+                onChange={handleChange}
+              />
 
-            <Button type="submit" css={{ width: "100%" }}>
-              Create Account
-            </Button>
-          </form>
-        </Card.Body>
-        <Card.Footer>
-          <div className="flex flex-row mx-auto">
-            <div>Already a user? &nbsp;</div>
-            <Link color="primary" target="_blank" href="/login">
-              Login here!
-            </Link>
-          </div>
-        </Card.Footer>
-      </Card>
+              <Button type="submit" css={{ width: "100%" }}>
+                Create Account
+              </Button>
+            </form>
+          </Card.Body>
+          <Card.Footer>
+            <div className="flex flex-row mx-auto">
+              <div>Already a user? &nbsp;</div>
+              <Link color="primary" target="_blank" href="/login">
+                Login here!
+              </Link>
+            </div>
+          </Card.Footer>
+        </Card>
+      </div>
     </Layout>
   );
 };
